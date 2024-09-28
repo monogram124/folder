fe26b1f2de92d2949f1e3e240cf82b0cb277048f
Monogram124 <maksimtimo09@gmail.com>
2024-09-29 00:03:52 +0300

    финальные корректировки в консект на 29.09.2024

--- py_learn/важные_темы.py
+++ py_learn/важные_темы.py
@@ -201,6 +201,95 @@ tom.meow()
 
 # ДАНДЕР МЕТОДЫ - позволяют более гибко работать со встроенными вещами в классе
 # свойства обозначаем в дандер методе __init__(иницлизация класса)
+# если не создали init в классе то нельзя передать аргументы
+
+# repr - дандер метод для програмистов который возвращает строку по которой видно и можно воссоздать состояние объекта (надо указывать состояние объекта Banknote(50))
+# str - для людей возврващает строку
+# eq - по умолчанию сравнивает адрес в памят в реадизации лучше сразу проверить тип чтобы не сравнивать апесльин с банкнотой 
+# если методы сравнения не реализованы то падает ошибка, везде там тожно нужно проверять нужный тип 
+# contains - для реализации проверки IN
+# если не реализован стр или репр то будет возвращен адрес в памяти 
+# по умолчанию проверка bool всегда вернет true, для изменения поведения прописывается дандер метод bool
+# len тоже вернет ошибку если не реализовать len дандер метод
+# дандер метод call делает объект класса callable, по сути в него можно записать любое поведение, в моем примере это сколько суммарно в кошельке денег
+# iter - возвращает объект итерратор, тот кто реализует итер = итерабл
+# next - должен вернуть след объект ищ контейнера а тот кто ЕГО реадизует = Итератор
+class Banknote:
+    def __init__(self, value: int):
+        self.value = value
+    
+    def __repr__(self): # типо для програмистов
+        return f"Banknote({self.value})"
+    
+    def __str__(self): # для людей
+        return f"Банкнота номиналом в {self.value} рублей"
+    
+    def __eq__(self, other): # без реализации этого метода питон будет сравнивать ссылки в памяти и выведет True только если это один и тот же объект
+        return self.value == other.value
+    
+    def __lt__(self, other):
+        return self.value < other.value 
+    
+    def __gt__(self, other):
+        return self.value > other.value 
+    
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+    
+class Wallet:
+    def __init__(self, *banknotes:Banknote):
+        self.container = []
+        self.container.append(banknotes)
+        self.index = 0
+        
+    def __repr__(self) -> str:
+        print(f"Wallet({self.container})")
+
+    def __contains__(self, item):
+        return item in self.container
+    
+    def __bool__(self):
+        return self.container
+    
+    def __len__(self):
+        return len(self.container)
+    
+    def __call__(self):
+        return f"{sum(e.value for e in self.container)} рублей"
+
+    def __iter__(self): # возвращает итерратор
+        return Iterator(self.container)
+
+    
+class Iterator:
+    def __init__(self, container):
+        self.container = container
+        self.index = 0
+
+    def __next__(self): # если класс реализует этот метод то он итератор но не итерабл
+        while self.index <= self.index < len(self.container):
+            value = self.container[self.index]
+            # тут просто реализиация метода next чтобы сделать этот класс итератором
+            self.index += 1
+            
+            return value
+        raise StopIteration # если выходит за пределы длины объекта бросаем исключение
+
+# iterable некоторый объект который возвращает итератор 
+# итерратор это тот объект который при вызове метода next возвращает один из своих элементов
+if __name__ == "__main__":
+    fifty = Banknote(50) 
+    hundred = Banknote(100)
+    print(f"{fifty!r}") # Banknote(50)
+    print(fifty) # Банкнота номиналом в 50 рублей
+
+    wallet = Wallet(fifty, hundred)
+    print(hundred in wallet) # True
+
+    print(wallet) # Wallet([Banknote(50), Banknote(100)])
 
 # НАСЛЕДОВАНИЕ
 
@@ -229,4 +318,4 @@ tom.meow()
 
 
 
-# написать про: дандер методы,  функции, *args и **kwargs, множества, рекурсия(именно как под копотом), наследование, сеттеры и геттеры
+# написать про: дандер методы,  функции, множества, рекурсия(именно как под копотом), наследование, сеттеры и геттеры
